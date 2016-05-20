321c5fa0e8ceabb8153315b461c634c93b3bf2b0
raracelly <arapuntocom@gmail.com>
2016-05-20 02:48:55 -0400

    listo

--- lab.c
+++ lab.c
@@ -11,11 +11,12 @@
 char *in;
 char *out;
 int n;
-
+int	** matriz;
 
 /* estructura de datos para entregar argumentos
 a las hebras que realizan dilatacion */
 struct dat{
+	int fila_inicial;
 	int cant_filas;
 	int id;
 };
@@ -28,51 +29,81 @@ void *dilatacion(void *arg){
 	datos = (struct dat*)arg;	
 
 	int cant_filas = datos->cant_filas;
+	int fila_inicial = datos->fila_inicial;
 	int id = datos->id;
+	
 	if(DEBUG)
 		printf("soy hebra %d, trabajo en %d filas\n",id,cant_filas);		
 	
-//posiciones a evaluar c/r al ppio del archivo	
-	int sup = (id-1)*cant_filas+1;
-	int inf = (id+1)*cant_filas+1;
-	int izq = id*cant_filas;
-	int der = id*cant_filas+2;
+//lectura a evaluar 
+	int lsup;
+	int linf;
+	int lizq;
+	int lder;
+//posicion puntero
+	int centro;
 
-//resultado de la evaluacion
-	int rsup;
-	int rinf;
-	int rizq;
-	int rder;
+//apertura de archivos
+	FILE *pf_in;
+	pf_in = fopen(in, "rb");
+
+	if(pf_in==NULL){
+		printf("hebra %d, Error al abrir imagen de entrada\n",id);
+		printf("hebra %d, Por precaución el programa es abortado.\n",id);
+		exit(EXIT_FAILURE);
+	}
 
 //lectura
 	int lectura;
-
-	FILE *pf_in;
-	fopen(in, "rb");
-
-	FILE *pf_out;
-	fopen(out, "wb");
-
 	int resultado;
-	int i,j;
-	for(i = 0; i < cant_filas; i++){
-		
-		for(j = 0; j < (n-1); j++){
-			
+	int f,c;
+	int aux = 0;
+	if(DEBUG)
+		printf("hebra %d, fila_inicial %d, posicion_inicial %d.\n",id,fila_inicial,n*fila_inicial);
+
+	for(f = 0; f < cant_filas; f++){	
+					
+		for(c = 0; c < n; c++){	
+
+			centro = n*fila_inicial+aux;
+			fseek(pf_in, centro*sizeof(int), SEEK_SET);//para leer centro
+			fread(&lectura, sizeof(int),1,pf_in);
+
+			if((id==0 && f==0) || (f+fila_inicial == n-1) || (c == 0) || (c == n-1)){	//caso primera y última fila del archivo, y primera y ultima columna
+				matriz[f+fila_inicial][c] = lectura;
+			}
+			else{	// dilatacion
+
+				fseek(pf_in, (centro-n)*sizeof(int), SEEK_SET); //posiciono para leer arriba
+				fread(&lsup, sizeof(int),1,pf_in);	//leo arriba
+
+				fseek(pf_in, (centro+n)*sizeof(int), SEEK_SET); //posiciono para leer abajo
+				fread(&linf, sizeof(int),1,pf_in);	//leo abajo
+
+				fseek(pf_in, (centro-1)*sizeof(int), SEEK_SET); //posiciono para leer izquierda
+				fread(&lizq, sizeof(int),1,pf_in);	//leo izquierda
+
+				fseek(pf_in, (centro+1)*sizeof(int), SEEK_SET); //posiciono para leer derecha
+				fread(&lder, sizeof(int),1,pf_in);	//leo derecha
+				
+				if(lsup == 1 || linf == 1 || lder == 1 || lizq == 1 || lectura == 1){	
+					resultado = 1;
+				}else{
+					resultado = 0;
+				}
+				matriz[f+fila_inicial][c] = resultado;
+			}
+				
+			aux++; //corrimiento del puntero central original
 		}
 	}
 	
 	fclose(pf_in);
-	fclose(pf_out);
-
 }
 
 
 int main (int argc, char* argv[]){
-//declaración variables de entrada 
-//	char *in; 	//imagen de entrada
-//	char *out; 	//imagen de salida
-//	int n; 		//ancho de imagen
+//declaración variables de entrada no globales
 	int h;		//cantidad de hebras
 	int debug;	//opción debug
 
@@ -90,12 +121,12 @@ int main (int argc, char* argv[]){
 	int option =0;	//opcion getopt
 	while( (option = getopt(argc, argv, "i:O:N:H:D:")) != -1){
 		switch(option){
-			case 'i': // imagen entrada.raw	
+			case 'i': // imagen entrada .raw	
 				in = optarg;			
 				if(DEBUG)
 					printf("imagen entrada: %s\n",in);
 				break;
-			case 'O': // imagen salida.raw
+			case 'O': // imagen salida .raw
 				out = optarg;
 				if(DEBUG)
 					printf("imagen salida: %s\n",out);
@@ -129,8 +160,6 @@ int main (int argc, char* argv[]){
 		printf("Error al abrir imagen de entrada\n");
 		printf("Por precaución el programa es abortado.\n");
 		exit(EXIT_FAILURE);
-	}else{
-		
 	}
 	if(n<0){
 		printf("el valor ingresado para \"ancho de imagen\" no es válido.\n");
@@ -142,33 +171,47 @@ int main (int argc, char* argv[]){
 		h = 1;
 		printf("se va a utilizar el valor por defecto: %d\n",h);
 	}
-	if(debug != 0 || debug != 1){
+	if(debug != 0 && debug != 1){
 		printf("el valor ingresado para \"debug\" no es válido.\n");
 		printf("no se va a realizar debug\n");	
+		debug = 0;
 	}
 
 //para asignar cant de filas por hebra
 	int cant_filas = n/h;
 	int resto = n%h;
 
-//arreglo de estructuras de datos para pasar a las hebras, y reserva de mem
+//para guardar el contenido de salida
+	int i,j;
+	matriz = (int**)malloc(n*sizeof(int*));
+	for(j = 0; j < n; j++){
+		matriz[j] = (int*)malloc(n*sizeof(int));			
+	}
+	
+
+//arreglo de estructuras de datos para pasar a las hebras, y su reserva de mem
 	struct dat *argumento;
 	argumento = (struct dat*)malloc(sizeof(struct dat)*h);
 	
-//arreglo de hebras y reserva de mem
+//arreglo de hebras y su reserva de mem
 	pthread_t *hebra;
 	hebra = (pthread_t*)malloc(sizeof(pthread_t)*h);
 
 //crear hebras
-	int i;
 	int rc;
-	for(i = 0; i < h; i++){ //for para crear las n hebras que harán sum.
+	int fila_inicial = 0;
+	for(i = 0; i < h; i++){ //for para crear las h hebras que harán dilatacion.
 		argumento[i].id = i+1;
 		argumento[i].cant_filas = cant_filas;		
 		if(resto > 0){
 			argumento[i].cant_filas = cant_filas + 1;
 			resto--;
-		}
+		}		
+		argumento[i].fila_inicial = fila_inicial;
+		if(DEBUG)
+			printf("para h%d, fila ini: %d\n",i+1,fila_inicial);
+		fila_inicial = fila_inicial + argumento[i].cant_filas; //calcula fila inicial para la sgte hebra
+
 		rc = pthread_create(&hebra[i], NULL, dilatacion, (void *) &argumento[i]);
 		if(rc != 0){
 			printf("error al crear h%d, rc = %d\n", i+1, rc);
@@ -178,12 +221,39 @@ int main (int argc, char* argv[]){
 //esperar finalización de las hebras
 	for(i = 0; i < h; i++){
 		pthread_join(hebra[i], NULL);
-		printf("Soy padre, hebra %d ha finalizado\n", i);
+		if(DEBUG)
+			printf("Soy padre, hebra %d ha finalizado\n", i+1);
 	}
-	
-	
-	
+
+//cerrar archivo que mantuvimos abierto durante la ejecución del programa
 	fclose(file_in);
+	
+//imprimir por consola
+	if(debug == 1){
+		for(i = 0; i < n; i++){
+			for(j = 0; j < n; j++){
+				printf("%d",matriz[i][j]);
+			}
+			printf("\n");
+		}	
+	}
+
+//padre escribe todo el archivo salida
+	file_out = fopen(out,"wb");
+
+	if(file_out==NULL){
+		printf("Error al crear imagen de salida\n");
+	}
+
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			fwrite(&matriz[i][j], sizeof(int),1,file_out);
+		}
+	}
+	fclose(file_out);
+
+	
+
 	return 0;
 }
 
